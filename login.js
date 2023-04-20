459496a6caf8ccfb1373a634fdcf57b431c7df88
NeCodz <m.esdras626@gmail.com>
2023-04-20 19:36:40 +0200

    Gestion de la connexion utilisateur

---
+++ login.js
@@ -0,0 +1,62 @@
+//RECUPERATION DES INFOS DE CONNEXION
+const loginForm = document.querySelector("#logRequest");
+const url = "http://localhost:5678/api/users/login";
+const email = document.querySelector("#email");
+const password = document.querySelector("#motdepasse");
+let logged = false
+let loginToken;
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  logginRequest()
+});
+
+async function logginRequest() {
+    const r = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+      }),
+      headers: {
+        accept: "application/json",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    if(r.ok == true){
+        // window.location.href = "./index.html"
+        return r.json()
+        .then(json =>{
+          loginToken = json.token
+          console.log(loginToken)
+          localStorage.setItem("token", loginToken)
+        })
+    }
+    else{
+        alert('Erreur dans l’identifiant ou le mot de passe')
+        throw new Error ("Impossible de se connecter")
+    }
+  }
+
+
+
+// loginForm.addEventListener('submit', function(e){
+//     e.preventDefault()
+//     fetch(url, {
+//         method: "POST",
+//         body: JSON.stringify({
+//             email: `${email.value}`,
+//             password: `${password.value}`
+//         }),
+//         headers: {
+//             "accept": "application/json",
+//             "Content-type": "application/json; charset=UTF-8"
+//         }
+//     })
+//     .then(response => response.json(), error => error.json())
+//     .then(json => {
+//         console.log(json)
+//         loginToken = json.token
+//         // window.location.href = "./index.html"
+//     })
+// })
